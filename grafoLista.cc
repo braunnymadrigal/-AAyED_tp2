801c7e8cd0d9634909949fc1dbda28db4f7d011a
braunnymadrigal <braunnyjmb@gmail.com>
2023-11-23 14:32:13 -0600

    grafoLista funciona correctamente

--- grafoLista.cc
+++ grafoLista.cc
@@ -9,33 +9,37 @@ void Grafo::Vaciar() { Destruir(); }
 bool Grafo::Vacio() { return primero == nullptr; }
 
 Vertice *Grafo::AgregarVert(int e) {
-  Vertice *v = new Vertice;
-  v->etiqueta = e;
-  v->abajo = nullptr;
-  v->sublista = nullptr;
-  Vertice *iterador = primero;
-  while (iterador != nullptr) {
-    iterador = iterador->abajo;
-  }
-  iterador = v;
-  return iterador;
+    Vertice *v = new Vertice;
+    v->etiqueta = e;
+    v->abajo = nullptr;
+    v->sublista = nullptr;
+    if (primero == nullptr) {
+        primero = v;
+    } else {
+        Vertice *iterador = primero;
+        while (iterador->abajo != nullptr) {
+            iterador = iterador->abajo;
+        }
+        iterador->abajo = v;
+    }
+    return v;
 }
 
 void Grafo::BorrarVert(Vertice *v) {
-  if (primero == v) {
-    Vertice *primAux = primero;
-    primero = primero->abajo;
-    delete primAux;
-  } else {
-    Vertice *anterior = primero;
-    Vertice *actual = primero->abajo;
-    while (actual != v) {
-      anterior = actual;
-      actual = actual->abajo;
+    if (primero == v) {
+        Vertice *primAux = primero;
+        primero = primero->abajo;
+        delete primAux;
+    } else {
+        Vertice *anterior = primero;
+        Vertice *actual = primero->abajo;
+        while (actual != v) {
+            anterior = actual;
+            actual = actual->abajo;
+        }
+        anterior->abajo = actual->abajo;
+        delete actual;
     }
-    anterior->abajo = actual->abajo;
-    delete actual;
-  }
 }
 
 void Grafo::ModifEtiq(Vertice *v, int e) { v->etiqueta = e; }
@@ -43,26 +47,26 @@ void Grafo::ModifEtiq(Vertice *v, int e) { v->etiqueta = e; }
 int Grafo::Etiq(Vertice *v) { return v->etiqueta; }
 
 void Grafo::AgregarArist(Vertice *v1, Vertice *v2, double p) {
-  AgregarAristPriv(v1, v2, p);
-  AgregarAristPriv(v2, v1, p);
+    AgregarAristPriv(v1, v2, p);
+    AgregarAristPriv(v2, v1, p);
 }
 
 void Grafo::BorrarArist(Vertice *v1, Vertice *v2) {
-  BorrarAristPriv(v1, v2);
-  BorrarAristPriv(v2, v1);
+    BorrarAristPriv(v1, v2);
+    BorrarAristPriv(v2, v1);
 }
 
 void Grafo::ModifPeso(Vertice *v1, Vertice *v2, double p) {
-  ModifPesoPriv(v1, v2, p);
-  ModifPesoPriv(v2, v1, p);
+    ModifPesoPriv(v1, v2, p);
+    ModifPesoPriv(v2, v1, p);
 }
 
 double Grafo::Peso(Vertice *v1, Vertice *v2) {
-  SubLista *iterador = v1->sublista;
-  while (iterador->vertice != v2) {
-    iterador = iterador->siguiente;
-  }
-  return iterador->peso;
+    SubLista *iterador = v1->sublista;
+    while (iterador->vertice != v2) {
+        iterador = iterador->siguiente;
+    }
+    return iterador->peso;
 }
 
 Vertice *Grafo::PrimVert() { return primero; }
@@ -70,119 +74,123 @@ Vertice *Grafo::PrimVert() { return primero; }
 Vertice *Grafo::SigVert(Vertice *v) { return v->abajo; }
 
 Vertice *Grafo::PrimVertAdy(Vertice *v) {
-  Vertice *vertDevolver = nullptr;
-  if (v->sublista != nullptr) {
-    vertDevolver = v->sublista->vertice;
-  }
-  return vertDevolver;
+    Vertice *vertDevolver = nullptr;
+    if (v->sublista != nullptr) {
+        vertDevolver = v->sublista->vertice;
+    }
+    return vertDevolver;
 }
 
 Vertice *Grafo::SigVertAdy(Vertice *v1, Vertice *v2) {
-  Vertice *vertADevolver = nullptr;
-  SubLista *iterador = v1->sublista;
-  while (iterador != nullptr) {
-    if (iterador->vertice == v2) {
-      iterador = iterador->siguiente;
-      if (iterador != nullptr) {
-        vertADevolver = iterador->vertice;
-      }
-      iterador = nullptr;
-    } else {
-      iterador = iterador->siguiente;
+    Vertice *vertADevolver = nullptr;
+    SubLista *iterador = v1->sublista;
+    while (iterador != nullptr) {
+        if (iterador->vertice == v2) {
+            iterador = iterador->siguiente;
+            if (iterador != nullptr) {
+                vertADevolver = iterador->vertice;
+            }
+            iterador = nullptr;
+        } else {
+            iterador = iterador->siguiente;
+        }
     }
-  }
-  return vertADevolver;
+    return vertADevolver;
 }
 
 int Grafo::NumVertices() {
-  int numero = 0;
-  Vertice *iterador = primero;
-  while (iterador != nullptr) {
-    ++numero;
-    iterador = iterador->abajo;
-  }
-  return numero;
+    int numero = 0;
+    Vertice *iterador = primero;
+    while (iterador != nullptr) {
+        ++numero;
+        iterador = iterador->abajo;
+    }
+    return numero;
 }
 
 // aux
 Vertice *Grafo::EtiqAVert(int e) {
-  Vertice *vertADevolver = nullptr;
-  Vertice *iterador = primero;
-  while (iterador != nullptr) {
-    if (iterador->etiqueta == e) {
-      vertADevolver = iterador;
-      iterador = nullptr;
-    } else {
-      iterador = iterador->abajo;
+    Vertice *iterador = primero;
+    bool devuelto = false;
+    while (iterador != nullptr && !devuelto) {
+        if (iterador->etiqueta == e) {
+            devuelto = true;
+        } else {
+            iterador = iterador->abajo;
+        }
     }
-  }
-  return vertADevolver;
+    return iterador;
 }
 
 void Grafo::ImprimirListas() {
-  Vertice *iteradorVert = primero;
-  while (iteradorVert != nullptr) {
-    std::cout << iteradorVert->etiqueta << ":";
-    SubLista *iteradorSub = iteradorVert->sublista;
-    while (iteradorSub != nullptr) {
-      std::cout << "\t" << iteradorSub->vertice->etiqueta << ","
-                << iteradorSub->peso;
-      iteradorSub = iteradorSub->siguiente;
+    Vertice *iteradorVert = primero;
+    while (iteradorVert != nullptr) {
+        std::cout << iteradorVert->etiqueta << ":";
+        SubLista *iteradorSub = iteradorVert->sublista;
+        while (iteradorSub != nullptr) {
+            std::cout << "\t" << (iteradorSub->vertice)->etiqueta << ","
+                      << iteradorSub->peso;
+            iteradorSub = iteradorSub->siguiente;
+        }
+        std::cout << std::endl;
+        iteradorVert = iteradorVert->abajo;
     }
-    std::cout << std::endl;
-    iteradorVert = iteradorVert->abajo;
-  }
 }
 
 void Grafo::Iniciar() { primero = nullptr; }
 
 void Grafo::Destruir() {
-  while (primero != nullptr) {
-    Vertice *primAux = primero;
-    SubLista *subL = primAux->sublista;
-    while (subL != nullptr) {
-      SubLista *subLAux = subL;
-      subL = subL->siguiente;
-      delete subLAux;
+    while (primero != nullptr) {
+        Vertice *primAux = primero;
+        SubLista *subL = primAux->sublista;
+        while (subL != nullptr) {
+            SubLista *subLAux = subL;
+            subL = subL->siguiente;
+            delete subLAux;
+        }
+        primero = primero->abajo;
+        delete primAux;
     }
-    primero = primero->abajo;
-    delete primAux;
-  }
-  primero = nullptr;
+    primero = nullptr;
 }
 
 void Grafo::AgregarAristPriv(Vertice *v1, Vertice *v2, double p) {
-  SubLista *subL = new SubLista;
-  subL->peso = p;
-  subL->siguiente = nullptr;
-  subL->vertice = v2;
-  SubLista *adyActual = v1->sublista;
-  while (adyActual != nullptr) {
-    adyActual = adyActual->siguiente;
-  }
-  adyActual = subL;
+    SubLista *subL = new SubLista;
+    subL->peso = p;
+    subL->siguiente = nullptr;
+    subL->vertice = v2;
+    if (v1->sublista == nullptr) {
+        v1->sublista = subL;
+    } else {
+        SubLista *adyActual = v1->sublista;
+        while (adyActual->siguiente != nullptr) {
+            adyActual = adyActual->siguiente;
+        }
+        adyActual->siguiente = subL;
+    }
 }
 
 void Grafo::BorrarAristPriv(Vertice *v1, Vertice *v2) {
-  if (v1->sublista->vertice == v2) {
-    delete v1->sublista;
-    v1->sublista = nullptr;
-  } else {
-    SubLista *anterior = v1->sublista;
-    SubLista *actual = anterior->siguiente;
-    while (actual->vertice != v2) {
-      anterior = actual;
-      actual = actual->siguiente;
+    if ((v1->sublista)->vertice == v2) {
+        SubLista *aux = v1->sublista;
+        v1->sublista = (v1->sublista)->siguiente;
+        delete aux;
+    } else {
+        SubLista *anterior = v1->sublista;
+        SubLista *actual = anterior->siguiente;
+        while (actual->vertice != v2) {
+            anterior = actual;
+            actual = actual->siguiente;
+        }
+        anterior->siguiente = actual->siguiente;
+        delete actual;
     }
-    anterior->siguiente == actual->siguiente;
-    delete actual;
-  }
 }
 
 void Grafo::ModifPesoPriv(Vertice *v1, Vertice *v2, double p) {
-  SubLista *iterador = v1->sublista;
-  while (iterador->vertice != v2) {
-    iterador = iterador->siguiente;
-  }
-  iterador->peso = p;
+    SubLista *iterador = v1->sublista;
+    while (iterador->vertice != v2) {
+        iterador = iterador->siguiente;
+    }
+    iterador->peso = p;
 }
--- grafoLista.hh
+++ grafoLista.hh
@@ -5,52 +5,52 @@
 struct SubLista;
 
 struct Vertice {
-  int etiqueta;
-  Vertice *abajo;
-  SubLista *sublista;
+    int etiqueta;
+    Vertice *abajo;
+    SubLista *sublista;
 };
 
 struct SubLista {
-  double peso;
-  SubLista *siguiente;
-  Vertice *vertice;
+    double peso;
+    SubLista *siguiente;
+    Vertice *vertice;
 };
 
 class Grafo {
 public:
-  // type alias
-  using Vert = Vertice *;
-  Vertice *VertNulo = nullptr;
+    // type alias
+    using Vert = Vertice *;
+    Vertice *VertNulo = nullptr;
 
 public:
-  Grafo();
-  ~Grafo();
-  void Vaciar();
-  bool Vacio();
-  Vertice *AgregarVert(int e);
-  void BorrarVert(Vertice *v);
-  void ModifEtiq(Vertice *v, int e);
-  int Etiq(Vertice *v);
-  void AgregarArist(Vertice *v1, Vertice *v2, double p);
-  void BorrarArist(Vertice *v1, Vertice *v2);
-  void ModifPeso(Vertice *v1, Vertice *v2, double p);
-  double Peso(Vertice *v1, Vertice *v2);
-  Vertice *PrimVert();
-  Vertice *SigVert(Vertice *v);
-  Vertice *PrimVertAdy(Vertice *v);
-  Vertice *SigVertAdy(Vertice *v1, Vertice *v2);
-  int NumVertices();
-  // aux
-  Vertice *EtiqAVert(int e);
-  void ImprimirListas();
+    Grafo();
+    ~Grafo();
+    void Vaciar();
+    bool Vacio();
+    Vertice *AgregarVert(int e);
+    void BorrarVert(Vertice *v);
+    void ModifEtiq(Vertice *v, int e);
+    int Etiq(Vertice *v);
+    void AgregarArist(Vertice *v1, Vertice *v2, double p);
+    void BorrarArist(Vertice *v1, Vertice *v2);
+    void ModifPeso(Vertice *v1, Vertice *v2, double p);
+    double Peso(Vertice *v1, Vertice *v2);
+    Vertice *PrimVert();
+    Vertice *SigVert(Vertice *v);
+    Vertice *PrimVertAdy(Vertice *v);
+    Vertice *SigVertAdy(Vertice *v1, Vertice *v2);
+    int NumVertices();
+    // aux
+    Vertice *EtiqAVert(int e);
+    void ImprimirListas();
 
 private:
-  void Iniciar();
-  void Destruir();
-  void AgregarAristPriv(Vertice *v1, Vertice *v2, double p);
-  void BorrarAristPriv(Vertice *v1, Vertice *v2);
-  void ModifPesoPriv(Vertice *v1, Vertice *v2, double p);
+    void Iniciar();
+    void Destruir();
+    void AgregarAristPriv(Vertice *v1, Vertice *v2, double p);
+    void BorrarAristPriv(Vertice *v1, Vertice *v2);
+    void ModifPesoPriv(Vertice *v1, Vertice *v2, double p);
 
 private:
-  Vertice *primero;
+    Vertice *primero;
 };
